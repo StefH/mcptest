7cdbe8cd3bc8f76462d7c08a3eb7c3a92fb5249c
Stef Heyenrath <stef.heyenrath@gmail.com>
2025-03-31 09:05:34 +0200

    mcptest

---
+++ src/mcptest/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Configuration;
+using ModelContextProtocol.Protocol.Transport;
+
+var config = new McpServerConfig
+{
+    Id = "everything",
+    Name = "everything",
+    TransportType = TransportTypes.StdIo,
+    TransportOptions = new Dictionary<string, string>
+    {
+        // dotnet tool install --version 0.0.1-preview-02 --global mcpserver.everything.stdio
+        { "command", "mcpserver.everything.stdio" }
+    }
+};
+
+var options = new McpClientOptions
+{
+    ClientInfo = new() { Name = "everything-client", Version = "1.0.0" }
+};
+
+var client = await McpClientFactory.CreateAsync(config, options, null, LoggerFactory.Create(c => c.AddConsole()));
+_ = await client.ListToolsAsync();
